948007812512984a21b8490470e2f30d7189a19b
CEMIL CANLI <cmlcnli34@gmail.com>
2022-05-29 04:10:44 +0300

    feat: add not found component if data empty

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./NotFound.module.scss";
+
+export default function NotFound() {
+  return (
+    <center>
+      <div className={styles.container}>
+        <h1>404!</h1>
+        <div className={styles.rick}>
+          <div className={styles.eyebrow}></div>
+          <div className={styles.eyeCover}></div>
+          <div className={`${styles.eye} ${styles.left}`}></div>
+          <div className={`${styles.eye} ${styles.right}`}></div>
+          <div className={styles.nose}></div>
+          <div className={styles.mouth}></div>
+          <svg
+            className={styles.saliva}
+            width="50px"
+            height="90px"
+            viewBox="0 0 49.13 90.16"
+          >
+            <path className={styles.cls - 1} />
+          </svg>
+          <div className={styles.saliva2}></div>
+        </div>
+      </div>
+    </center>
+  );
+}

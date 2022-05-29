e422f70d6304c48ea31e8e1c49e368667ecf7f06
CEMIL CANLI <cmlcnli34@gmail.com>
2022-05-29 04:08:07 +0300

    feat: create home page and episodes page

---
+++ src/Pages/Episodes.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card/Card";
+import InputGroup from "../components/Filter/category/InputGroup";
+
+const Episodes = () => {
+  let [results, setResults] = React.useState([]);
+  let [info, setInfo] = useState([]);
+  let { air_date, name } = info;
+  let [id, setID] = useState(1);
+
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    fetch(api).then((res) => {
+      res.json().then((data) => {
+        setInfo(data);
+        Promise.all(
+          data.characters.map(async (x) => {
+            return fetch(x).then((res) => res.json());
+          })
+        ).then((res) => {
+          setResults(res);
+        });
+      });
+    });
+  }, [api]);
+
+  return (
+    <div className="container">
+      <div className="row mb-3">
+        <h1 className="text-center mb-3 text-light">
+          Episode name :{" "}
+          <span className="text-primary">{name === "" ? "Unknown" : name}</span>
+        </h1>
+        <h5 className="text-center text-light">
+          Air Date: {air_date === "" ? "Unknown" : air_date}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 col-12">
+          <div className="row">
+            <Card page="/episodes/" results={results} />
+          </div>
+        </div>
+        <div className="col-lg-3 col-12 mb-4 text-light">
+          <h4 className="text-center mb-4">Pick Episode</h4>
+          <InputGroup name="Episode" changeID={setID} total={51} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Episodes;

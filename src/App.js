f9422aa0d8237aabe5b1936853a006919f39c19c
CEMIL CANLI <cmlcnli34@gmail.com>
2022-05-29 04:07:14 +0300

    feat: create router and init app js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React from "react";
+import Home from "./Pages/Home";
+
+import Navbar from "./components/Navbar/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import CardDetails from "./components/Card/CardDetails";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="background">
+        <div className="App">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<CardDetails />} />
+
+          <Route path="/episodes" element={<Episodes />} />
+          <Route path="/episodes/:id" element={<CardDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
